406253bb3db2d7eb2f85e0c4248985de93c1e526
unknown <cahlee73@gmail.com>
2018-09-30 23:22:02 +0900

    first commit

---
+++ src/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,32 @@
+package springbook.user.dao;
+
+import java.sql.SQLException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import springbook.user.domain.User;
+
+public class UserDaoTest {
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		ApplicationContext context = 
+				new GenericXmlApplicationContext("applicationContext.xml");
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		
+		User user = new User();
+		user.setId("ctrl0703");
+		user.setName("이창희");
+		user.setPassword("asdf");
+		
+		dao.add(user);
+		
+		System.out.println(user.getId() + " 등록 성공");
+		
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		
+		System.out.println(user2.getId() + " 조회 성공");
+	}
+}
