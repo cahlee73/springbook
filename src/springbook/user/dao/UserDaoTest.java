1483b0ebe1bc28167d3a64a713fb6121ef086af4
unknown <cahlee73@gmail.com>
2018-10-03 01:51:12 +0900

    chapter2

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -1,32 +1,75 @@
 package springbook.user.dao;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.sql.SQLException;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import springbook.user.domain.User;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/test-applicationContext.xml")
 public class UserDaoTest {
+
+	@Autowired
+	private UserDao dao;
+	private User user1 = new User("ctrl0703", "이창희", "asdf");
+	private User user2 = new User("ctrl0704", "창레기", "1234");
+	private User user3 = new User("cahlee", "이대리", "1q2w3e");
 	
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		ApplicationContext context = 
-				new GenericXmlApplicationContext("applicationContext.xml");
-		UserDao dao = context.getBean("userDao", UserDao.class);
+	@Before
+	public void setUp() {
+		this.user1 = new User("ctrl0703", "이창희", "asdf");
+		this.user2 = new User("ctrl0704", "창레기", "1234");
+		this.user3 = new User("cahlee", "이대리", "1q2w3e");
+	}
+	
+	@Test
+	public void addAndGet() throws SQLException, ClassNotFoundException {		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
 		
-		User user = new User();
-		user.setId("ctrl0703");
-		user.setName("이창희");
-		user.setPassword("asdf");
+		dao.add(user1);
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
 		
-		dao.add(user);
+		User userget1 = dao.get(user1.getId());
+		assertThat(userget1.getName(), is(user1.getName()));
+		assertThat(userget1.getPassword(), is(user1.getPassword()));
 		
-		System.out.println(user.getId() + " 등록 성공");
+		User userget2 = dao.get(user2.getId());
+		assertThat(userget2.getName(), is(user2.getName()));
+		assertThat(userget2.getPassword(), is(user2.getPassword()));
+	}
+	
+	@Test
+	public void count() throws ClassNotFoundException, SQLException {		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
 		
-		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		System.out.println(user2.getPassword());
+		dao.add(user1);
+		assertThat(dao.getCount(), is(1));
 		
-		System.out.println(user2.getId() + " 조회 성공");
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		dao.add(user3);
+		assertThat(dao.getCount(), is(3));
+	}
+	
+	@Test(expected=EmptyResultDataAccessException.class)
+	public void getUserFailure() throws ClassNotFoundException, SQLException {
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.get("unknown_id");
 	}
 }
--- src/springbook/user/domain/User.java
+++ src/springbook/user/domain/User.java
@@ -5,23 +5,37 @@ public class User {
 	String name;
 	String password;
 	
-	public String getId() {
-		return id;
+	public User() {
 	}
-	public void setId(String id) {
+
+	public User(String id, String name, String password) {
+		super();
 		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
 		this.name = name;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
 		this.password = password;
 	}
 	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
